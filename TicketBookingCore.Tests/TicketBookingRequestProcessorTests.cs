381f8e9a1cf2cb209217f9e891ea4ddaceba72d6
MikaelGille <mikael.gille@gmail.com>
2025-02-04 18:38:37 +0100

    Add project files.

---
+++ TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketBookingCore.Tests
+{
+    public class TicketBookingRequestProcessorTests
+    {
+        [Fact]
+        public void ShouldReturnTicketBookingResultWithRequestValues()
+        {
+            // Arrange
+            var processor = new TicketBookingRequestProcessor();
+
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Mikael",
+                LastName = "Gille",
+                Email = "mikael.gille@gmail.com"
+            };
+
+            // Act
+            TicketBookingResponse response = processor.Book(request);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
+        }
+    }
+}
---
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TicketBookingCore
+{
+    public class TicketBookingRequestProcessor
+    {
+        public TicketBookingRequestProcessor()
+        {
+        }
+
+        public TicketBookingResponse Book(TicketBookingRequest request)
+        {
+            //  Refractor
+            return new TicketBookingResponse
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            };
+        }
+    }
+}
